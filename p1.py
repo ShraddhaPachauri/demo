81e7e46b488da42fcdba0cc4010098105887c44b
ShraddhaPachauri <shraddha.pachauri12345@gmail.com>
2024-05-07 13:56:15 +0530

    demo

---
+++ p1.py
@@ -0,0 +1,5 @@
+#  List out Practice Question using if-else 
+#
+# 1. Write a program to check if a number is even or odd
+# 2. Write a program to check if a number is positive or negative
+# 3. Write a program to check if a number is divisible by 5 or not
